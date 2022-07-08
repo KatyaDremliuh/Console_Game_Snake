a2cc60b887de80242df30389d51504efad6ef9fc
Katsiaryna Dremliuh <nerona.91@gmail.com>
2022-07-08 20:33:07 +0300

    Draw border of the field

--- Console_Game_Snake/HorizontalLine.cs
+++ Console_Game_Snake/HorizontalLine.cs
@@ -2,26 +2,18 @@
 
 namespace Console_Game_Snake
 {
-    public class HorizontalLine
+    public class HorizontalLine : Line
     {
         public List<Point> PointsList { get; }
 
-        public HorizontalLine(int xLeft, int xRight, int y, char symbol)
+        public HorizontalLine(int xLeft, int xRight, int oY, char symbol)
         {
             PointsList = new List<Point>();
 
             for (int x = xLeft; x <= xRight; x++)
             {
-                Point point = new Point(x, y, symbol);
+                Point point = new Point(x, oY, symbol);
                 PointsList.Add(point);
-            }
-        }
-
-        public void DrawHorizontalLine()
-        {
-            foreach (Point point in PointsList)
-            {
-                point.DrawPoint();
             }
         }
     }
